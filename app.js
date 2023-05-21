4f6eb8c9f6cb32ff87ce3c67093f921b6b1f98f6
duela <duelapower@gmail.com>
2023-05-21 23:47:52 +0200

    add About page and include about.ejs

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.static('public'));
 
 // to retrieve parsed data from browser to server
 app.use(bodyParser.urlencoded({extended:true}));
+
 // creating the first get root to send the user Hello when the user access the home root
 app.get('/', function(req, res) {
 
@@ -20,57 +21,6 @@ app.get('/', function(req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  //inside the call back we use res.render and ahe index
-  // using if else statement
-  // if (currentDay === 1){
-  //   day = "Monday";
-  // }
-  // else if (currentDay === 2) {
-  //   day = "Tuesday";
-  // }
-  // else if (currentDay === 3) {
-  //   day = "Wednesday";
-  // }
-  // else if (currentDay === 4) {
-  //   day = "Thursday";
-  // }
-  // else if (currentDay === 5) {
-  //   day = "Friday";
-  // }
-  // else if (currentDay === 6) {
-  //   day = "Saturday";
-  // }
-  // else {
-  //   day = "Sunday";
-  //
-  // }
-
-  // using switch
-  // switch (currentDay) {
-  //   case 0:
-  //     day = "Sunday";
-  //     break;
-  //     case 1:
-  //       day = "Monday";
-  //       break;
-  //       case 2:
-  //         day = "Tuesday";
-  //         break;
-  //         case 3:
-  //           day = "Wednesday";
-  //           break;
-  //           case 4:
-  //             day = "Thursday";
-  //             break;
-  //             case 5:
-  //               day = "Friday";
-  //               break;
-  //               case 0:
-  //                 day = "Saturday";
-  //                 break;
-  //   default:
-  //   console.log("Error date")
-  // }
 
   res.render('index', {listTitle: day, newLists: addLists});
   //res.send('Hello');  // res.send parse data from the server to the browser, allowing us to perform logic on how server
@@ -86,9 +36,15 @@ app.post('/', function(req, res){
     res.redirect('/');
   }
 });
+//Sending the index.ejs file to the browser as soon as a request is made on localhost:3000 to local route localhost:3000/work
 app.get('/work', function(req,res){
   res.render('index', {listTitle: "Work List", newLists: workItems});
 });
+
+//Sending the about.ejs file to the browser as soon as a request is made on localhost:3000 to local route localhost:3000/about
+app.get('/about', function(req, res){
+  res.render('about');
+})
 
 
 
@@ -96,3 +52,56 @@ app.get('/work', function(req,res){
 app.listen(port, function(){
   console.log("server is now connected to port: " + port);
 });
+
+
+//inside the call back we use res.render and ahe index
+// using if else statement
+// if (currentDay === 1){
+//   day = "Monday";
+// }
+// else if (currentDay === 2) {
+//   day = "Tuesday";
+// }
+// else if (currentDay === 3) {
+//   day = "Wednesday";
+// }
+// else if (currentDay === 4) {
+//   day = "Thursday";
+// }
+// else if (currentDay === 5) {
+//   day = "Friday";
+// }
+// else if (currentDay === 6) {
+//   day = "Saturday";
+// }
+// else {
+//   day = "Sunday";
+//
+// }
+
+// using switch
+// switch (currentDay) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//       case 2:
+//         day = "Tuesday";
+//         break;
+//         case 3:
+//           day = "Wednesday";
+//           break;
+//           case 4:
+//             day = "Thursday";
+//             break;
+//             case 5:
+//               day = "Friday";
+//               break;
+//               case 0:
+//                 day = "Saturday";
+//                 break;
+//   default:
+//   console.log("Error date")
+// }
