f1c5f058cec144d3dd9588197d0652a38767e59c
duela <duelapower@gmail.com>
2023-05-21 21:56:36 +0200

    Add work root with extra features to create a work list

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-var ejs = require('ejs');
-var addLists = ["Buy Food","Cook Food", "Eat Food"];
+const ejs = require('ejs');
+let addLists = ["Buy Food","Cook Food", "Eat Food"];
+let workItems = [];
 // set app view engine to ejs : always plca eit below the declaration
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // to retrieve parsed data from browser to server
 app.use(bodyParser.urlencoded({extended:true}));
 // creating the first get root to send the user Hello when the user access the home root
 app.get('/', function(req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "numertic"
-  }
-  var day = today.toLocaleDateString("en-Us");
-  
+
+  let options = { weekday: 'long', month: 'long', day: 'numeric' };
+  let today  = new Date();
+
+  let day = today.toLocaleDateString("en-US", options);
+
   //inside the call back we use res.render and ahe index
   // using if else statement
   // if (currentDay === 1){
@@ -72,16 +72,25 @@ app.get('/', function(req, res) {
   //   console.log("Error date")
   // }
 
-  res.render('index', {kindOfDay: day, newLists: addLists});
+  res.render('index', {listTitle: day, newLists: addLists});
   //res.send('Hello');  // res.send parse data from the server to the browser, allowing us to perform logic on how server
+});
+app.post('/', function(req, res){
+  let item = req.body.newList;
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect('/work');
+  }
+  else {
+    addLists.push(item);
+    res.redirect('/');
+  }
+});
+app.get('/work', function(req,res){
+  res.render('index', {listTitle: "Work List", newLists: workItems});
 });
 
 
-app.post('/', function(req, res){
-  var addList = req.body.newList;
-  addLists.push(addList);
-  res.redirect('/');
-})
 
 // We listen on prot 300 and console log the message
 app.listen(port, function(){
